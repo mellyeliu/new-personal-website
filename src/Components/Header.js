3a01e04ab390218aeedd46f7e8c5b782a31ffbf9
Melissa Liu <mellyeliu@meta.com>
2023-07-26 17:19:43 -0700

    remove embarassing shit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -25,7 +25,6 @@ class Header extends Component {
         var imageSrc = 'images/'+photo.image;
         return  <div key={photo.date} className="container">
           <img id="headerpic" src={imageSrc}></img>
-          {/* <button onClick={this.toggleButton} className="top-left hvr-grow">PLAY &nbsp;<i class="fas fa-backward"></i>&nbsp; {this.state.isActive ? (<span><i class="fas fa-pause"></i></span>): (<span> &#9658;</span>)} &nbsp;<i class="fas fa-forward"></i></button> */}
         <div className="bottom-left">{photo.place} <br/>{photo.date}</div>
         </div>
       }, this)
@@ -33,20 +32,15 @@ class Header extends Component {
     return (
       <header id="home">
       <Nav data={this.props.data} title='MELISSA LIU' subtitle='Code / Writing / Art'/>
-
-      <Fade top duration={2000}>
       <div onClick={this.toggleButton}  className="banner">
           <div className="container" style={{zIndex: 1000000}}>
             <div className="top-left">{!this.state.isActive ? (<span id="play-button">PAUSED &nbsp;<i style={{fontSize: 8}} class="fas fa-pause"></i></span>): (<span id="play-button">PLAY &nbsp;<i style={{fontSize: 8}} class="fas fa-play"></i></span>)}</div>
-          {/* <div className="top-left">PLAY &nbsp;<button onClick={{p}}> <i class="fas fa-backward"></i></button> <button onClick={this.toggleButton}> {this.state.isActive ? (<i style={{fontSize: 9, paddingRight: 4}} class="fas fa-pause"></i>) : (<i style={{fontSize: 8, paddingRight: 2}} class="fas fa-play"></i> )} </button>&nbsp;<i class="fas fa-forward"></i></div> */}
         </div>
           <Carousel autoplay={this.state.isActive} wrapAround={true} transitionMode='fade'
-          withoutControls={true} pauseOnHover={false} autoplayInterval={4000} enableKeyboardControls={true}>
+          withoutControls={true} pauseOnHover={false} autoplayInterval={3000} enableKeyboardControls={true}>
             {art}
           </Carousel>
       </div>
-      </Fade>
-
    </header>
     );
   }
