a9746ad42ba72474afd41242f9311a3198850d50
Melissa Liu <mellye.liu@gmail.com>
2024-04-08 09:38:44 -0700

    resize image for loading

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -106,7 +106,7 @@ class Header extends Component {
     }
     return (
       <><Nav data={this.props.data} title='Mellye.liu' subtitle='Code / Writing / Art' /><header id="home">
-        <Fade duration={1000} delay={500}>
+        <Fade duration={2000} delay={500}>
           <div className="banner" >
             <div className="container" style={{ zIndex: 1 }}>
               <div onClick={this.toggleButton} className="top-left">{
